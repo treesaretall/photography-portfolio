960113150c0ad83f26f98e574fce0bc6c89f6875
treesaretall <himatthewhurst@hotmail.com>
2023-09-02 15:04:22 -0400

    swapped out img, finished layout of home

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import woodsbnw from "../img/about/woodsbnw.jpg";
+
+import { Link } from "react-router-dom";
+
 const About = () => {
-  return <section className="section bg-yellow-300">About</section>;
+  return (
+    <section className="section ">
+      <div className="container mx-auto h-full relative">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          <div>image</div>
+          <div>text</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,18 @@ const Home = () => {
         <div className="flex flex-col justify-center">
           <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
             <h1 className="h1">Photographer</h1>
+            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+              Orlando, FL
+            </p>
+            <Link to={"/contact"} className="btn mb-[30px] text-[14px]">
+              Hire Me
+            </Link>
           </div>
-          <div>Image</div>
+          <div className="flex justify-end max-h-96 lg:max-h-max">
+            <div className="relative lg:-right-40 overflow-hidden">
+              <img src={mebnw} alt="Matt in black and white" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
